60fe0d0fe183827167e41837dc79896058b6ef21
Yjason-K <gomgom0324@gmail.com>
2023-10-01 19:31:38 +0900

    style: 임시로 헤더 색상 지정

--- Frontend/src/pages/Place.tsx
+++ Frontend/src/pages/Place.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
 
 const Place = () => {
+  // 여행지 이름을 parameter로 가져오기
+  // or 여행지 좌표 등등 여행지 정보기반의 parameter로 설정
   const { id } = useParams();
+
   return <div>여행지 표시 페이지 Query Parameter기반 {id}</div>;
 };
 
