f4d5d129a451ab9a336f4573e780455befab08fe
onlyjoon <nhj7334@icloud.com>
2023-10-08 22:31:38 +0900

    add : Swagger 설정 변경

--- Backend/travel/src/main/java/welcome/travel/controller/OauthController.java
+++ Backend/travel/src/main/java/welcome/travel/controller/OauthController.java
@@ -1,5 +1,7 @@
 package welcome.travel.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "oauth", description = "소셜로그인 API")
 public class OauthController {
     private final AuthService authService;
 
     @GetMapping("/login/oauth2/callback/kakao")
-    public KakaoLoginResponseDto kakaoLogin(HttpServletRequest request) {
-        String code = request.getParameter("code");
+    @Operation(summary = "카카오 로그인", description = "카카오 연동 로그인")
+    public KakaoLoginResponseDto kakaoLogin(@RequestParam String code) {
         KakaoTokenDto kakaoAccessToken = authService.getKakaoAccessToken(code);
         return authService.kakaoLogin(kakaoAccessToken);
     }
 
     @PostMapping("/login/oauth2/kakao")
+    @Operation(summary = "카카오 로그인", description = "카카오 회원 문자 인증 및 동의 처리")
     public ResponseEntity<?> socialLogin(@RequestHeader("Authorization") String token, @RequestBody SocialLoginRequestDto socialLoginRequestDto) {
         authService.processAfterKakao(token, socialLoginRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
