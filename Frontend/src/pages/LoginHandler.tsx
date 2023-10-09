e6a78ef79bf95467c6f55d362ad99e9d98b67471
Yjason-K <gomgom0324@gmail.com>
2023-10-10 00:26:06 +0900

    fix: 카카오 소셜 로그인 로직 변경

--- Frontend/src/pages/LoginHandler.tsx
+++ Frontend/src/pages/LoginHandler.tsx
@@ -3,7 +3,7 @@ import Loading from "../components/common/Loading";
 import { useNavigate } from "react-router-dom";
 import "../styles/pages/LoginHandler.css";
 import { useDispatch } from "react-redux";
-import { kakaologin, kakaoSlice } from "../lib/redux/store";
+import { kakaologin, kakaoSlice, loginSlice } from "../lib/redux/store";
 import { useEffect } from "react";
 
 const LoginHandler = () => {
@@ -24,15 +24,17 @@ const LoginHandler = () => {
     })
       .then((res) => {
         //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-        console.log(res.data);
         localStorage.setItem("token", res.data.tokenInfo.accessToken);
         // flag확인하하고 route설정하고  카카오 로그인 true로 변경
         // flag: true 회원가입페이지로 이동
         if (res.data.flag) {
+          // 회원가입에서 step2까지만 진행하기 위해 지정
           dispatch(kakaoSlice.actions.kakaologin(true));
+          // 회원가입 페이지 이동해서
           navigate("/signup", { replace: true });
         } else {
           // db에 데이터가 저장돼있는 경우
+          dispatch(loginSlice.actions.login());
           navigate("/main", { replace: true });
         }
       })
@@ -42,7 +44,6 @@ const LoginHandler = () => {
   };
 
   useEffect(() => {
-    console.log("effect 실행");
     kakaoLogin();
   }, [code]);
 
