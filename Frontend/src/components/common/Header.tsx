99939f302e2851b2ab6099f419deaab7f650b70c
osy9757 <osy9757@gmail.com>
2023-10-16 09:55:52 +0900

    FEAT : LOGIN 상태에 따라 헤더 오른쪽 부분 출력상태 변경

--- Frontend/src/components/common/Header.tsx
+++ Frontend/src/components/common/Header.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "../../lib/types/redux";
 import '../../styles/common/Header.css';
 
 const Header: React.FC = () => {
   const [menuActive, setMenuActive] = useState(false);
+  const isLogin = useSelector((state: RootState) => state.login.value);
+
   const toggleMenu = () => {
     setMenuActive(!menuActive);
   };
@@ -28,25 +32,29 @@ const Header: React.FC = () => {
       </div>
 
       <div className="navbar__right">
-        <ul className={`navbar__menu ${menuActive ? 'active' : ''}`}>
-          <li className='dropdown'>
-            <a href="#">Login</a>
-            <div className='dropdown-options'>
-              <a href="/login">로그인</a>
-              <a href="/signup">회원가입</a>
-              <a href="/findpwd">아이디<br></br>비밀번호찾기</a>
-            </div>
-          </li>
-          <li className='dropdown'>
-            <a href="#">Signin</a>
-            <div className='dropdown-options'>
-              <a href="/login">로그인</a>
-              <a href="/signup">회원가입</a>
-              <a href="/findpwd">아이디<br></br>비밀번호찾기</a>
-            </div>
-          </li>
-        </ul>
+          <ul className={`navbar__menu ${menuActive ? 'active' : ''}`}>
+              {isLogin ? (
+                  <li className='dropdown'>
+                      <a href="#">Login</a>
+                      <div className='dropdown-options'>
+                          <a href="/login">로그인</a>
+                          <a href="/signup">회원가입</a>
+                          <a href="/findpwd">아이디<br></br>비밀번호찾기</a>
+                      </div>
+                  </li>
+              ) : (
+                  <li className='dropdown'>
+                      <a href="#">Signin</a>
+                      <div className='dropdown-options'>
+                          <a href="/login">로그인</a>
+                          <a href="/signup">회원가입</a>
+                          <a href="/findpwd">아이디<br></br>비밀번호찾기</a>
+                      </div>
+                  </li>
+              )}
+          </ul>
       </div>
+
 
       <a href="#" className="navbar__toggleBtn" onClick={toggleMenu}>☰</a>
     </nav>
