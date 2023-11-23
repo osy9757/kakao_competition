f3e0ea7109f20a150d0ca3614bd9c9fe31da0fb5
only-juun <nhj7334@icloud.com>
2023-11-23 19:38:11 +0900

    refactor - 코드 수정

--- Backend/travel/src/main/java/welcome/travel/domain/Account.java
+++ Backend/travel/src/main/java/welcome/travel/domain/Account.java
@@ -3,6 +3,8 @@ package welcome.travel.domain;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 @Builder
 public class Account {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long kakaoId;
     private String email;
