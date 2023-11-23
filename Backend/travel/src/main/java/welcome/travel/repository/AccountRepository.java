ae638308cefd91006d6d814407032cdbf607f6c5
only-juun <nhj7334@icloud.com>
2023-11-23 10:52:14 +0900

    refactor - 코드 수정

--- Backend/travel/src/main/java/welcome/travel/repository/AccountRepository.java
+++ Backend/travel/src/main/java/welcome/travel/repository/AccountRepository.java
@@ -1,10 +1,7 @@
 package welcome.travel.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import welcome.travel.domain.Account;
 
-@Repository
-public interface AccountRepository extends JpaRepository<Account,Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
